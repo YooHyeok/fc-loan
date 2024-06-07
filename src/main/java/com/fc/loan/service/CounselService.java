c54bbc6c782f1d4424dfae1328fd5f1e16b56ac0
webdevyoo <yjk7454@naver.com>
2024-06-08 00:29:00 +0900

    Ch.02-03: `CounselService` - 상담 도메인을 위한 Service 인터페이스 및 등록 기능 추상메소드 추가

---
+++ src/main/java/com/fc/loan/service/CounselService.java
@@ -0,0 +1,9 @@
+package com.fc.loan.service;
+
+import com.fc.loan.dto.CounselDTO;
+
+import static com.fc.loan.dto.CounselDTO.*;
+
+public interface CounselService {
+    Response create(Request request);
+}
