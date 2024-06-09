300bfa4ee007c060759d9d5568c4bef60f10284f
webdevyoo <yjk7454@naver.com>
2024-06-10 00:31:11 +0900

    Ch.02-05: `CounselService` - 상담 서비스 인터페이스 수정 기능 추상메소드 추가

--- src/main/java/com/fc/loan/service/CounselService.java
+++ src/main/java/com/fc/loan/service/CounselService.java
@@ -7,4 +7,5 @@ import static com.fc.loan.dto.CounselDTO.*;
 public interface CounselService {
     Response create(Request request);
     Response get(Long counselId);
+    Response update(Long counselId, Request request);
 }
