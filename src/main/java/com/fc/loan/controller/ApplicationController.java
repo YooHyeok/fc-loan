9ea9afd7632ba874e78e20c00461a72ae78292c1
webdevyoo <yjk7454@naver.com>
2024-06-12 01:00:46 +0900

    Ch.03-03: `ApplicationController` - 신청 도메인 컨트롤러 신청 조회 API 핸들러 매핑 API 구현

---
+++ src/main/java/com/fc/loan/controller/ApplicationController.java
@@ -0,0 +1,24 @@
+package com.fc.loan.controller;
+
+import com.fc.loan.dto.ResponseDTO;
+import com.fc.loan.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import static com.fc.loan.dto.ApplicationDTO.*;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/applications")
+public class ApplicationController extends AbstractController{
+
+    private final ApplicationService counselService;
+
+    @PostMapping
+    public ResponseDTO<Response> create(@RequestBody Request request) {
+        return ok(counselService.create(request)); // AbstractController에 정의된 ok 메소드 호출
+    }
+
+
+}
