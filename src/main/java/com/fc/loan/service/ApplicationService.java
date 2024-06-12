cb7ae3ccbebbf8ba91e5d6e54bd8cb26937da77b
webdevyoo <yjk7454@naver.com>
2024-06-13 00:34:39 +0900

    Ch.03-05: `ApplicationService` - 신청 서비스 인터페이스 수정 기능 추상메소드 추가

--- src/main/java/com/fc/loan/service/ApplicationService.java
+++ src/main/java/com/fc/loan/service/ApplicationService.java
@@ -6,5 +6,6 @@ import static com.fc.loan.dto.ApplicationDTO.Response;
 public interface ApplicationService {
     Response create(Request request);
     Response get(Long applicationId);
+    Response update(Long applicationId, Request request);
 
 }
