64463e8ae2bca12fc8b3099176e013bdca058daf
webdevyoo <yjk7454@naver.com>
2024-06-16 00:15:03 +0900

    Ch.03-08: `TermsController` - 이용약관 서비스 컨트롤러 전체 조회 API 핸들러 매핑 API 구현

--- src/main/java/com/fc/loan/controller/TermsController.java
+++ src/main/java/com/fc/loan/controller/TermsController.java
@@ -3,10 +3,9 @@ package com.fc.loan.controller;
 import com.fc.loan.dto.ResponseDTO;
 import com.fc.loan.service.TermsService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static com.fc.loan.dto.TermsDTO.*;
 
@@ -20,5 +19,10 @@ public class TermsController extends AbstractController{
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
         return ok(termsService.create(request));
+    }
+
+    @GetMapping
+    public ResponseDTO<List<Response>> getAll() {
+        return ok(termsService.getAll());
     }
 }
