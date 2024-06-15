f968fc8913cc913cf27a71bd5fd8b4fd6ae4b424
webdevyoo <yjk7454@naver.com>
2024-06-16 00:14:38 +0900

    Ch.03-08: `TermsService` - 이용약관 서비스 인터페이스 전체 조회 기능 추상메소드 추가

--- src/main/java/com/fc/loan/service/TermsService.java
+++ src/main/java/com/fc/loan/service/TermsService.java
@@ -1,9 +1,12 @@
 package com.fc.loan.service;
 
+import java.util.List;
+
 import static com.fc.loan.dto.TermsDTO.Request;
 import static com.fc.loan.dto.TermsDTO.Response;
 
 public interface TermsService {
     Response create(Request request);
+    List<Response> getAll();
 
 }
