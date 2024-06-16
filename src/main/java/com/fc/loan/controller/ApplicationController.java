e98053878997f1d747320efb0f58a9ba34dbe154
webdevyoo <yjk7454@naver.com>
2024-06-17 01:58:57 +0900

    Ch.03-09: `ApplicationController` - 대출 신청 동의 약관 등록 API 핸들러 매핑 API 구현

--- src/main/java/com/fc/loan/controller/ApplicationController.java
+++ src/main/java/com/fc/loan/controller/ApplicationController.java
@@ -1,6 +1,5 @@
 package com.fc.loan.controller;
 
-import com.fc.loan.dto.ApplicationDTO;
 import com.fc.loan.dto.ResponseDTO;
 import com.fc.loan.service.ApplicationService;
 import lombok.RequiredArgsConstructor;
@@ -14,27 +13,31 @@ import static com.fc.loan.dto.ApplicationDTO.*;
 @RequestMapping("/applications")
 public class ApplicationController extends AbstractController{
 
-    private final ApplicationService counselService;
+    private final ApplicationService applicationService;
 
     @PostMapping
     public ResponseDTO<Response> create(@RequestBody Request request) {
-        return ok(counselService.create(request)); // AbstractController에 정의된 ok 메소드 호출
+        return ok(applicationService.create(request)); // AbstractController에 정의된 ok 메소드 호출
     }
 
     @GetMapping("/{applicationId}")
     public ResponseDTO<Response> create(@PathVariable Long applicationId) {
-        return ok(counselService.get(applicationId)); // AbstractController에 정의된 ok 메소드 호출
+        return ok(applicationService.get(applicationId)); // AbstractController에 정의된 ok 메소드 호출
     }
 
     @PutMapping("/{applicationId}")
     public ResponseDTO<Response> update(@PathVariable Long applicationId, @RequestBody Request request) {
-        return ok(counselService.update(applicationId, request)); // AbstractController에 정의된 ok 메소드 호출
+        return ok(applicationService.update(applicationId, request)); // AbstractController에 정의된 ok 메소드 호출
     }
 
     @DeleteMapping("/{applicationId}")
     public ResponseDTO<Response> delete(@PathVariable Long applicationId) {
-        counselService.delete(applicationId);
+        applicationService.delete(applicationId);
         return ok(); // AbstractController에 정의된 ok 메소드 호출
     }
 
+    @PostMapping("/{applicationId}/terms")
+    public ResponseDTO<Boolean> create(@PathVariable Long applicationId, @RequestBody AcceptTerms request) {
+        return ok(applicationService.acceptTerms(applicationId, request)); // AbstractController에 정의된 ok 메소드 호출
+    }
 }
