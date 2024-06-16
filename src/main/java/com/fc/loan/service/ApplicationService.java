bc65defb282885b84dc5abab421922ae12809b3c
webdevyoo <yjk7454@naver.com>
2024-06-17 00:30:20 +0900

    Ch.03-09: `ApplicationService` - 대출 신청 서비스 인터페이스 동의약관 엔티티 약관동의 저장 기능 추상화

--- src/main/java/com/fc/loan/service/ApplicationService.java
+++ src/main/java/com/fc/loan/service/ApplicationService.java
@@ -1,12 +1,18 @@
 package com.fc.loan.service;
 
-import static com.fc.loan.dto.ApplicationDTO.Request;
-import static com.fc.loan.dto.ApplicationDTO.Response;
+
+
+
+import static com.fc.loan.dto.ApplicationDTO.*;
 
 public interface ApplicationService {
+    /* --- 대출 신청 --- */
     Response create(Request request);
     Response get(Long applicationId);
     Response update(Long applicationId, Request request);
 
     void delete(Long applicationId);
+
+    /* --- 대출 신청 이용 약관 --- */
+    Boolean acceptTerms(Long applicationId, AcceptTerms request); // 약관 동의
 }
