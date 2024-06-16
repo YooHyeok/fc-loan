b39266bcd622cb9c3ea9670ff4c0bf96cf7f08d5
webdevyoo <yjk7454@naver.com>
2024-06-17 01:58:18 +0900

    Ch.03-09: `ApplicatonServiceTest` - 대출 신청 테스트 동의 약관 등록/에러 목 테스트 추가

--- src/test/java/com/fc/loan/service/ApplicationServiceTest.java
+++ src/test/java/com/fc/loan/service/ApplicationServiceTest.java
@@ -1,11 +1,16 @@
 package com.fc.loan.service;
 
+import com.fc.loan.domain.AcceptTerms;
 import com.fc.loan.domain.Application;
 import com.fc.loan.domain.Counsel;
+import com.fc.loan.domain.Terms;
+import com.fc.loan.dto.ApplicationDTO;
 import com.fc.loan.exception.BaseException;
 import com.fc.loan.exception.ResultType;
+import com.fc.loan.repository.AcceptTermsRepository;
 import com.fc.loan.repository.ApplicationRepository;
 import com.fc.loan.repository.CounselRepository;
+import com.fc.loan.repository.TermsRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,8 +18,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.domain.Sort;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 import static com.fc.loan.dto.ApplicationDTO.*;
@@ -27,6 +35,10 @@ class ApplicationServiceTest {
 
     @Mock
     private ApplicationRepository applicationRepository;
+    @Mock
+    private TermsRepository termsRepository;
+    @Mock
+    private AcceptTermsRepository acceptTermsRepository;
 
     @Spy // ModelMapper는 서로 다른 오브젝트를 매핑해주는 유틸성 객체이므로 따로 모킹처리 없이 역할 자체를 순수하게 하기 위해서 @Spy로 지정한다
     private ModelMapper modelMapper;
@@ -126,6 +138,112 @@ class ApplicationServiceTest {
 
         //then
         Assertions.assertThat(entity.getIsDeleted()).isSameAs(true);
+
+    }
+
+    @Test
+    @DisplayName("대출신청의 동의약관에 대한 요청이 왔을 때 동의 약관을 추가한다.")
+    void Should_AddAcceptTerms_When_RequestAcceptTermsOfApplication() throws Exception {
+        //given
+        Terms entityA = Terms.builder()
+                .termsId(1L)
+                .name("약관 1")
+                .termsDetailUrl("https://abcd.efg1")
+                .build();
+
+        Terms entityB = Terms.builder()
+                .termsId(2L)
+                .name("약관 2")
+                .termsDetailUrl("https://abcd.efg2")
+                .build();
+
+        // applicationId
+        Long applicationIdId = 1L;
+
+        // 1번과 2번의 약관을 동의하여 RequestDTO인 AcceptTerms 정적 중첩 요청객체에 담아 전달
+        ApplicationDTO.AcceptTerms request =
+                ApplicationDTO.AcceptTerms.builder()
+                .acceptTermsIds(Arrays.asList(1L, 2L)) // 약관 1, 2
+                .build();
+
+        // Mocking!
+        Mockito.when(applicationRepository.findById(applicationIdId)).thenReturn(Optional.ofNullable(new Application()));
+        Mockito.when(termsRepository.findAll(Sort.by(Sort.Direction.ASC, "termsId"))).thenReturn(List.of(entityA, entityB));
+//        Mockito.when(acceptTermsRepository.save(ArgumentMatchers.any(AcceptTerms.class))).thenReturn(new AcceptTerms()); // 반복문으로 할경우 이렇게 테스트
+        Mockito.when(acceptTermsRepository.saveAll(ArgumentMatchers.anyList())).thenReturn(List.of(new AcceptTerms(), new AcceptTerms())); // save All
+
+        //when
+        Boolean actual = applicationService.acceptTerms(applicationIdId, request);
+
+        //then
+        Assertions.assertThat(actual).isTrue();
+    }
+
+    @Test
+    @DisplayName("대출신청의 동의약관에 대해 2개중 1개의 약관에 대한 요청이 왔을 때 예외를 던진다.")
+    void Should_ThrowsException_When_RequestNotAcceptTermsOfApplication() throws Exception {
+        //given
+        Terms entityA = Terms.builder()
+                .termsId(1L)
+                .name("약관 1")
+                .termsDetailUrl("https://abcd.efg1")
+                .build();
+
+        Terms entityB = Terms.builder()
+                .termsId(2L)
+                .name("약관 2")
+                .termsDetailUrl("https://abcd.efg2")
+                .build();
+
+        // applicationId
+        Long applicationIdId = 1L;
+
+        // 1번과 2번의 약관을 동의하여 RequestDTO인 AcceptTerms 정적 중첩 요청객체에 담아 전달
+        ApplicationDTO.AcceptTerms request =
+                ApplicationDTO.AcceptTerms.builder()
+                        .acceptTermsIds(Arrays.asList(1L)) // 약관 1개만 동의했을때 Exception이 발생할것이다.
+                        .build();
+
+        // Mocking! - 저장까지 가지 못하고 Exception을 Throw하므로 saveAll에 대한 Mocking을 해서는 안된다.
+        Mockito.when(applicationRepository.findById(applicationIdId)).thenReturn(Optional.ofNullable(new Application()));
+        Mockito.when(termsRepository.findAll(Sort.by(Sort.Direction.ASC, "termsId"))).thenReturn(List.of(entityA, entityB));
+
+        // when & then - Throws (Exception 발생!)
+        org.junit.jupiter.api.Assertions.assertThrows(BaseException.class, () -> applicationService.acceptTerms(applicationIdId, request));
+
+    }
+
+    @Test
+    @DisplayName("대출신청의 동의약관에 대한 존재하지 않는 약관 요청이 왔을 때 예외를 던진다.")
+    void Should_ThrowsException_When_RequestNotExistAcceptTermsOfApplication() throws Exception {
+        //given
+        Terms entityA = Terms.builder()
+                .termsId(1L)
+                .name("약관 1")
+                .termsDetailUrl("https://abcd.efg1")
+                .build();
+
+        Terms entityB = Terms.builder()
+                .termsId(2L)
+                .name("약관 2")
+                .termsDetailUrl("https://abcd.efg2")
+                .build();
+
+        // applicationId
+        Long applicationIdId = 1L;
+
+        // 1번과 2번의 약관을 동의하여 RequestDTO인 AcceptTerms 정적 중첩 요청객체에 담아 전달
+        ApplicationDTO.AcceptTerms request =
+                ApplicationDTO.AcceptTerms.builder()
+                        .acceptTermsIds(Arrays.asList(1L, 3L)) // 약관 1개만 동의했을때 Exception이 발생할것이다.
+                        .build();
+
+        // Mocking! - 저장까지 가지 못하고 Exception을 Throw하므로 saveAll에 대한 Mocking을 해서는 안된다.
+        Mockito.when(applicationRepository.findById(applicationIdId)).thenReturn(Optional.ofNullable(new Application()));
+        Mockito.when(termsRepository.findAll(Sort.by(Sort.Direction.ASC, "termsId"))).thenReturn(List.of(entityA, entityB));
+
+        // when & then - Throws (Exception 발생!)
+        org.junit.jupiter.api.Assertions.assertThrows(BaseException.class, () -> applicationService.acceptTerms(applicationIdId, request));
 
     }
 }
