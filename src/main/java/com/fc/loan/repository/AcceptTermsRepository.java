3cf2da880359cf11f221623a6744710ab6bdbf30
webdevyoo <yjk7454@naver.com>
2024-06-17 00:30:26 +0900

    Ch.03-09: `AcceptTermsRepository` - 대출 신청 별 동의한 약관 정보 관리용 동의약관 리포지토리 추가

---
+++ src/main/java/com/fc/loan/repository/AcceptTermsRepository.java
@@ -0,0 +1,7 @@
+package com.fc.loan.repository;
+
+import com.fc.loan.domain.AcceptTerms;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcceptTermsRepository extends JpaRepository<AcceptTerms, Long> {
+}
