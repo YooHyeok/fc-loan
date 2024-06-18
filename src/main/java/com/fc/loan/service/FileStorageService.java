235d6e914c2cdd0830d235723cf41cb784cbf229
webdevyoo <yjk7454@naver.com>
2024-06-19 00:00:34 +0900

    Ch.03-11: `FileStorageService` - 대출 신청 서류 도메인 서비스 인터페이스 조회 기능 추상화

--- src/main/java/com/fc/loan/service/FileStorageService.java
+++ src/main/java/com/fc/loan/service/FileStorageService.java
@@ -1,7 +1,11 @@
 package com.fc.loan.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 public interface FileStorageService {
     void save(MultipartFile[] files);
+    Resource load(String fileName);
 }
