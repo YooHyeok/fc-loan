90b056d10d4bc65137e1391ca7df024ce074b85f
webdevyoo <yjk7454@naver.com>
2024-06-20 00:08:37 +0900

    extra: `FileStorageService` - 대출 신청 서류 도메인 서비스 인터페이스 전체 조회(zip) 기능 추상화
    디렉토리 경로에 존재하는 입회 서류에 대한 다중 파일들을 
    zip 확장자로 압축한 뒤
    한번에 다운로드 할 수 있는 기능 추상화

--- src/main/java/com/fc/loan/service/FileStorageService.java
+++ src/main/java/com/fc/loan/service/FileStorageService.java
@@ -3,9 +3,12 @@ package com.fc.loan.service;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.nio.file.Path;
 import java.util.List;
+import java.util.stream.Stream;
 
 public interface FileStorageService {
     void save(MultipartFile[] files);
     Resource load(String fileName);
+    Resource loadAsZip(String[] fileNames);
 }
