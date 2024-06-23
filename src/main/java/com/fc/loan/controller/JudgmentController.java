0d55304659ae8d433b87f92ac9e9d0b0fd434259
webdevyoo <yjk7454@naver.com>
2024-06-24 00:48:11 +0900

    Ch.04-03: `JudgementController` - 대출 심사 저장 API 핸들러 매핑 API 구현

---
+++ src/main/java/com/fc/loan/controller/JudgmentController.java
@@ -0,0 +1,26 @@
+package com.fc.loan.controller;
+
+import com.fc.loan.dto.JudgmentDTO;
+import com.fc.loan.dto.ResponseDTO;
+import com.fc.loan.service.JudgmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.fc.loan.dto.JudgmentDTO.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/judgments")
+public class JudgmentController extends AbstractController{
+
+    private final JudgmentService judgmentService;
+
+    @PostMapping
+    public ResponseDTO<Response> create (@RequestBody Request request) {
+        return ok(judgmentService.create(request));
+    }
+
+}
