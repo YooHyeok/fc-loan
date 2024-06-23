5e49df5e15584e1d2df4dc83aa638584ef8ba2f1
webdevyoo <yjk7454@naver.com>
2024-06-24 00:45:00 +0900

    Ch.04-02: `JudgementService` - 대출 심사 도메인 서비스 인터페이스 추가 및 추가기능  추상화

---
+++ src/main/java/com/fc/loan/service/JudgmentService.java
@@ -0,0 +1,11 @@
+package com.fc.loan.service;
+
+import java.util.List;
+
+import static com.fc.loan.dto.JudgmentDTO.Request;
+import static com.fc.loan.dto.JudgmentDTO.Response;
+
+public interface JudgmentService {
+    Response create(Request request);
+
+}
