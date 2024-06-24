cebdb30497fc14b2092ace1cdeae75d69bb5f300
webdevyoo <yjk7454@naver.com>
2024-06-25 02:14:18 +0900

    Ch.04-04: `JudgementController` - 대출 심사 pk기준 조회 API 핸들러 매핑 API 구현

--- src/main/java/com/fc/loan/controller/JudgmentController.java
+++ src/main/java/com/fc/loan/controller/JudgmentController.java
@@ -4,10 +4,7 @@ import com.fc.loan.dto.JudgmentDTO;
 import com.fc.loan.dto.ResponseDTO;
 import com.fc.loan.service.JudgmentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.fc.loan.dto.JudgmentDTO.*;
 
@@ -21,6 +18,11 @@ public class JudgmentController extends AbstractController{
     @PostMapping
     public ResponseDTO<Response> create (@RequestBody Request request) {
         return ok(judgmentService.create(request));
+    }
+
+    @GetMapping("/{judgmentId}")
+    public ResponseDTO<Response> get(@PathVariable Long judgmentId) {
+        return ok(judgmentService.get(judgmentId));
     }
 
 }
