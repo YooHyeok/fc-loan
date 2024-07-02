d3047d3a79f1deae232491c139040a91aac1011c
webdevyoo <yjk7454@naver.com>
2024-07-02 13:50:11 +0900

    Ch.04-07: `judgmentService` - 대출심사 도메인 서비스 인터페이스 대출심사 금액 부여 기능 추상화

--- src/main/java/com/fc/loan/service/JudgmentService.java
+++ src/main/java/com/fc/loan/service/JudgmentService.java
@@ -1,6 +1,6 @@
 package com.fc.loan.service;
 
-import java.util.List;
+import com.fc.loan.dto.ApplicationDTO;
 
 import static com.fc.loan.dto.JudgmentDTO.Request;
 import static com.fc.loan.dto.JudgmentDTO.Response;
@@ -11,5 +11,8 @@ public interface JudgmentService {
     Response getJudgmentOfApplication(Long applicationId);
     Response update(Long judgmentId, Request request);
     void delete(Long judgmentId);
+
+    ApplicationDTO.GrantAmount grant(Long judgmentId); // GrantAmount 어떠한 금액이 어떤 신청정보에 최종적으로 부여됐다 라는 의미
+
 
 }
