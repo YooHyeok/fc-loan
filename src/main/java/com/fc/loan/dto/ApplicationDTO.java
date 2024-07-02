4fd22bc214d3a7750bf072c57d5e2b380ca3fff7
webdevyoo <yjk7454@naver.com>
2024-07-02 13:48:50 +0900

    Ch.04-07: `ApplicationDTO.GrantAmount` - 대출심사 금액 부여 기능용 ResponseDTO 추가

--- src/main/java/com/fc/loan/dto/ApplicationDTO.java
+++ src/main/java/com/fc/loan/dto/ApplicationDTO.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -37,6 +38,12 @@ public class ApplicationDTO implements Serializable {
 
     }
 
+    /**
+     * Ch.03-09 대출 신청 별 동의한 약관 정보 관리를 위한 동의약관 DTO(Request)
+     * 이중 관계로 인한 중간 테이블 구성으로 DTO를 구성해야 한다.
+     * 실제 약관 동의시 동의한 약관들을 List로 받는다.
+     * 실제 신청정보에 반영되므로 ApplicationDTO에 생성한다.
+     */
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
@@ -46,4 +53,23 @@ public class ApplicationDTO implements Serializable {
         List<Long> acceptTermsIds;
 
     }
+
+    /**
+     * Ch.04-07 대출 심사 금액 부여 기능을 위한 금액부여 DTO(Response)
+     * 어떠한 금액이 어떤 신청정보에 최종적으로 부여됐다 라는 의미
+     * 실제 신청정보에 반영되므로 ApplicationDTO에 생성한다.
+     */
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class GrantAmount {
+        private Long applicationId; // 어떤 신청 정보에 대해 반영 되는가.
+        private BigDecimal approvalAmount; // 얼마가 승인이 되는가
+        private LocalDateTime createdAt; // 신청정보 생성 일자.
+        private LocalDateTime updatedAt; // 신청정보 수정 일자.
+
+    }
+
 }
