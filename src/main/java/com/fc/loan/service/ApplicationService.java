6a6ed6a88f59fd5249478f030c169f997acb02aa
webdevyoo <yjk7454@naver.com>
2024-07-04 00:32:49 +0900

    Ch.05-04: `ApplicationService` - 대출 신청 도메인 서비스 대출 계약 기능 추상화

--- src/main/java/com/fc/loan/service/ApplicationService.java
+++ src/main/java/com/fc/loan/service/ApplicationService.java
@@ -1,8 +1,5 @@
 package com.fc.loan.service;
 
-
-
-
 import static com.fc.loan.dto.ApplicationDTO.*;
 
 public interface ApplicationService {
@@ -15,4 +12,7 @@ public interface ApplicationService {
 
     /* --- 대출 신청 이용 약관 --- */
     Boolean acceptTerms(Long applicationId, AcceptTerms request); // 약관 동의
+
+    /* --- Ch.05-04 대출 계약 기능 (대출 집행전) --- */
+    Response contract(Long applicationId);
 }
