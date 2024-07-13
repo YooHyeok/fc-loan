704fa564b15651ed71249fcd481cd93afb2aed96
webdevyoo <yjk7454@naver.com>
2024-07-13 13:13:50 +0900

    Ch.05-05: `BalanceDTO` 대출 집행 도메인 `대출 잔고` 요청/응답 DTO 추가
    대출 집행시 대출 잔고가 저장되고 대출 상환시에 대출잔고에서 상환에 해당하는 대출 ID를 기준으로 대출잔고 금액이 차감된다.

---
+++ src/main/java/com/fc/loan/dto/BalanceDTO.java
@@ -0,0 +1,34 @@
+package com.fc.loan.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class BalanceDTO implements Serializable {
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    @Builder
+    public static class Request {
+        private Long applicationId; // 어떤 신청정보의 상환인지
+        private BigDecimal entryAmount; // 집행금액
+    }
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Response {
+
+        private Long balanceId;
+        private Long applicationId;
+        private BigDecimal balance;
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+    }
+}
