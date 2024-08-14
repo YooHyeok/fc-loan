1a1774cf23a3473bf789980a493111ab6a7a6b55
webdevyoo <yjk7454@naver.com>
2024-08-15 00:30:54 +0900

    Ch.05-07: `BalanceServiceImpl` - 대출 집행 도메인 `대출 잔고` 서비스 클래스 대출 잔고 수정 기능 구체화
    As-Is 에서 To-Be 로 수정된다.
    즉, 이전 잔고를 0원으로 초기화하고, 수정하려는 잔고로 변경한다.
    따라서 이전 잔고와 이후 잔고를 요청 객체로 함께 전달 받는다

--- src/main/java/com/fc/loan/service/BalanceServiceImpl.java
+++ src/main/java/com/fc/loan/service/BalanceServiceImpl.java
@@ -9,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
 import static com.fc.loan.dto.BalanceDTO.*;
 
 @Service
@@ -20,6 +22,7 @@ public class BalanceServiceImpl implements BalanceService {
 
     @Override
     public Response create(Long applicationId, Request request) {
+        /* null 처리만 하고 반환하진 않는다. */
         if (balanceRepository.findByApplicationId(applicationId).isPresent()) {
             throw new BaseException(ResultType.SYSTEM_ERROR);
         }
@@ -30,5 +33,25 @@ public class BalanceServiceImpl implements BalanceService {
 
         Balance saved = balanceRepository.save(balance);
         return modelMapper.map(saved, Response.class);
+    }
+
+    @Override
+    public Response update(Long applicationId, UpdateRequest request) {
+        //balance - isPresent처리와 같으나 값을 반환까지 해준다.
+        Balance balance = balanceRepository.findByApplicationId(applicationId).orElseThrow(() -> {
+            throw new BaseException(ResultType.SYSTEM_ERROR);
+        });
+
+        BigDecimal beforeEntryAmount = request.getBeforeEntryAmount();
+        BigDecimal afterEntryAmount = request.getAfterEntryAmount();
+
+        //as-is -> to-be : 밸런스(잔고) 값 보정
+        BigDecimal updatedBalance = balance.getBalance();
+        updatedBalance = updatedBalance
+                            .subtract(beforeEntryAmount) // 과거 집행되었던 금액 차감 (잔고를 0원으로 만든다)
+                            .add(afterEntryAmount); // 현재 수정하고자 하는 금액 증감
+        balance.setBalance(updatedBalance);
+
+        return modelMapper.map(balanceRepository.save(balance), Response.class); // 수정-저장 후 DTO변환 및 반환
     }
 }
