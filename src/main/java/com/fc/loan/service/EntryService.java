d5de9dc7e9124f1225f21457f2d283134799c76b
webdevyoo <yjk7454@naver.com>
2024-08-15 00:31:24 +0900

    Ch.05-07: `EntryService` - 대출 집행 도메인 서비스 인터페이스 대출 집행 수정 기능 추상화

--- src/main/java/com/fc/loan/service/EntryService.java
+++ src/main/java/com/fc/loan/service/EntryService.java
@@ -11,4 +11,7 @@ public interface EntryService {
 
     /* 대출 집행 조회 기능 */
     Response get(Long applicationId);
+
+    /* 대출 집행 수정 기능 */
+    UpdateResponse update(Long entryId, Request request);
 }
