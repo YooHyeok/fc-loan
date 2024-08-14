cf8909f98f2ddb6e46a27fd0663d4897951a31c3
webdevyoo <yjk7454@naver.com>
2024-08-15 00:30:10 +0900

    Ch.05-07: `BalanceService` - 대출 집행 도메인 `대출 잔고` 서비스 인터페이스 대출 잔고 수정 기능 추상화

--- src/main/java/com/fc/loan/service/BalanceService.java
+++ src/main/java/com/fc/loan/service/BalanceService.java
@@ -1,7 +1,11 @@
 package com.fc.loan.service;
 
+import java.math.BigDecimal;
+
 import static com.fc.loan.dto.BalanceDTO.*;
 
 public interface BalanceService {
     Response create(Long applicationId, Request request);
+
+    Response update(Long applicationId, UpdateRequest request);
 }
