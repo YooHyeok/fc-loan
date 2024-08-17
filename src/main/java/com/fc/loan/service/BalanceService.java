598d2871b7ad613130269d1590363d66e9799f73
webdevyoo <yjk7454@naver.com>
2024-08-18 00:51:59 +0900

    Ch.06-03: `BalanceService` - 대출 상환 등록기능 대출 잔고 도메인 서비스 인터페이스 수정 기능 추상화

--- src/main/java/com/fc/loan/service/BalanceService.java
+++ src/main/java/com/fc/loan/service/BalanceService.java
@@ -7,5 +7,9 @@ import static com.fc.loan.dto.BalanceDTO.*;
 public interface BalanceService {
     Response create(Long applicationId, Request request);
 
+    /* 대출 `집행` 등록/삭제: 수정(추가/삭제)용 */
     Response update(Long applicationId, UpdateRequest request);
+
+    /* 대출 `상환` 등록/삭제: 수정(추가/삭제)용 */
+    Response repaymentUpdate(Long applicationId, RepaymentRequest request);
 }
