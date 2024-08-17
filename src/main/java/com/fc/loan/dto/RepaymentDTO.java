9c8822caf157ea6a7a26a6c413f65d9b4dba2643
webdevyoo <yjk7454@naver.com>
2024-08-18 00:44:43 +0900

    Ch.06-03: `RepaymentDTO` - 대출 상환 도메인 기본 요청/응답 DTO 정의

---
+++ src/main/java/com/fc/loan/dto/RepaymentDTO.java
@@ -0,0 +1,31 @@
+package com.fc.loan.dto;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RepaymentDTO implements Serializable {
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Request {
+        private BigDecimal repaymentAmount; // 상환할 금액
+    }
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Builder
+    @Getter
+    @Setter
+    public static class Response {
+        private Long applicationId;
+        private BigDecimal repaymentAmount; // 상환된 금액
+        private BigDecimal balance; //상환 후 잔고
+        private LocalDateTime createdAt;
+        private LocalDateTime updatedAt;
+
+    }
+}
