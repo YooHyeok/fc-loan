2ec2c858f4245c89ac5a3c9c03b18d6c2d2155bc
webdevyoo <yjk7454@naver.com>
2024-08-19 00:02:06 +0900

    Ch.06-04: `RepaymentRepository` 대출 상환 도메인 레포지토리 대출 신청 ID 기준 조회 기능 추가

--- src/main/java/com/fc/loan/repository/RepaymentRepository.java
+++ src/main/java/com/fc/loan/repository/RepaymentRepository.java
@@ -1,8 +1,12 @@
 package com.fc.loan.repository;
 
 import com.fc.loan.domain.Repayment;
+import com.fc.loan.dto.RepaymentDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface RepaymentRepository extends JpaRepository<Repayment, Long> {
 
+    List<Repayment> findAllByApplicationId(Long applicationId);
 }
