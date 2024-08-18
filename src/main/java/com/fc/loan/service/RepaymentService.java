79ed7a5c0ddd4bf51bd7c0c1002f3c36e05e7d97
webdevyoo <yjk7454@naver.com>
2024-08-19 00:01:59 +0900

    Ch.06-04: `RepaymentService` - 대출 상환 도메인 서비스 인터페이스 대출 상환 조회 기능 추상화

--- src/main/java/com/fc/loan/service/RepaymentService.java
+++ src/main/java/com/fc/loan/service/RepaymentService.java
@@ -2,9 +2,12 @@ package com.fc.loan.service;
 
 import com.fc.loan.dto.RepaymentDTO;
 
+import java.util.List;
+
 import static com.fc.loan.dto.RepaymentDTO.*;
 
 public interface RepaymentService {
     Response create(Long applicationId, Request request);
+    List<ListResponse> get(Long applicationId);
 
 }
